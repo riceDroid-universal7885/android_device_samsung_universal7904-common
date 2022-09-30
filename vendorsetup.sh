0698766edd28bf3092bd9c90634615b8b83e1f48
eun0115 <gianpaoloestacio5@gmail.com>
2022-09-30 21:48:18 +0800

    universal7904: Update vendorsetup.sh
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,7 @@
 git clone https://github.com/riceDroid-universal7885/android_device_samsung_m20lte device/samsung/m20lte -b lineage-20
 git clone https://github.com/SamarV-121/android_device_samsung_m20lte-kernel device/samsung/m20lte-kernel
 git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy -b lineage-20
-rm -rf hardware/samsung
-git clone https://github.com/LineageOS/android_hardware_samsung hardware/samsung -b lineage-20
+git clone https://github.com/riceDroid-universal7885/android_hardware_samsung hardware/samsung
 git clone https://github.com/LineageOS/android_hardware_samsung_nfc hardware/samsung/nfc
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt -b lineage-20
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal hardware/samsung_slsi/scsc_wifibt/wifi_hal -b lineage-20
